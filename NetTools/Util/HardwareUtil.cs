e423e066d68526c77aef74a810264bc99551ce41
camark <camark@live.cn>
2018-11-01 11:37:30 +0800

    init

---
+++ NetTools/Util/HardwareUtil.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Management;
+using System.Net;
+using System.Text;
+
+namespace NetTools.Util
+{
+    public class HardwareUtil
+    {
+        public string GethostName()
+        {
+            return Dns.GetHostName();
+        }
+
+        public static string GetCPUSerialNumber()
+        {
+            string empty = string.Empty;
+            ManagementClass managementClass = new ManagementClass("Win32_Processor");
+            ManagementObjectCollection instances = managementClass.GetInstances();
+            using (ManagementObjectCollection.ManagementObjectEnumerator enumerator = instances.GetEnumerator())
+            {
+                if (enumerator.MoveNext())
+                    empty = enumerator.Current["ProcessorId"].ToString();
+            }
+            managementClass.Dispose();
+            instances.Dispose();
+            return empty;
+        }
+
+        public static string GetDiskSerialNumber()
+        {
+            ManagementObjectCollection.ManagementObjectEnumerator enumerator = new ManagementObjectSearcher()
+            {
+                Query = ((ObjectQuery)new SelectQuery("Win32_DiskDrive", "", new string[2]
+                {
+                "PNPDeviceID",
+                "Signature"
+                }))
+            }.Get().GetEnumerator();
+            enumerator.MoveNext();
+            return enumerator.Current.Properties["signature"].Value.ToString().Trim();
+        }
+
+        public static string GetMoAddress()
+        {
+            string str = " ";
+            using (ManagementClass managementClass = new ManagementClass("Win32_NetworkAdapterConfiguration"))
+            {
+                foreach (ManagementObject instance in managementClass.GetInstances())
+                {
+                    if ((bool)instance["IPEnabled"])
+                        str = instance["MacAddress"].ToString();
+                    instance.Dispose();
+                }
+            }
+            return str.ToString();
+        }
+    }
+}
---
+++ NetTools/Util/MessageUtil.cs
@@ -0,0 +1,27 @@
+﻿using System.Windows.Forms;
+
+namespace NetTools.Util
+{
+    public class MessageUtil
+    {
+        public static void ShowWarning(string msg, string title = "警告")
+        {
+            MessageBox.Show(msg, title, MessageBoxButtons.OK, MessageBoxIcon.Warning);
+        }
+
+        public static void ShowError(string msg, string title = "错误")
+        {
+            MessageBox.Show(msg, title, MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+
+        public static void ShowInfo(string msg, string title = "信息")
+        {
+            MessageBox.Show(msg, title, MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        public static bool ShowQuestion(string msg, string title = "询问")
+        {
+            return MessageBox.Show(msg, title, MessageBoxButtons.OKCancel, MessageBoxIcon.Question) == DialogResult.OK;
+        }
+    }
+}
